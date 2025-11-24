730ee740a8913455fb95aec48ae16a6e9ed8bed9
mammut21 <mathiasgotte@gmx.de>
2025-11-24 12:21:27 +0100

    Update db_fkt.sql

--- db_fkt.sql
+++ db_fkt.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE MACRO seconds_to_hhmmss(sec) AS (
       COALESCE(
         try_cast(
-            lpad((((sec::DOUBLE) / 3600)::int)::text, 2, '0') || ':' ||
-            lpad((((((sec::DOUBLE) % 3600) / 60)::int)::text), 2, '0') || ':' ||
+            lpad((((sec::DOUBLE) // 3600)::int)::text, 2, '0') || ':' ||
+            lpad((((((sec::DOUBLE) % 3600) // 60)::int)::text), 2, '0') || ':' ||
             lpad(((sec::DOUBLE)::INT % 60)::text, 2, '0') as VARCHAR
            ) ,
         sec::text
